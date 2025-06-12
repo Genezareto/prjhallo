2923c8505cc8aa47633d4b1241ac5286606258b6
Genezareto <genezaretokaijeputraguntur@gmail.com>
2025-06-12 09:19:53 +0800

    Penambahan Nama

--- hallo.cpp
+++ hallo.cpp
@@ -3,31 +3,15 @@
 using namespace std;
 
 int main() {
-    double tugas,uts,uas,rata;
-    cout<<"Input Nilai Tugas";
-    cin>>tugas;
-    cout<<"Input Nilai UTS";
-    cin>>uts;
-    cout<<"Input Nilai UAS";
-    cin>>uas;
-    rata=((tugas*0.20)+(uts*0.30)+(uas*0.50));
-    if(rata>100){
-        cout<<"Nilai terlalu tinggi";
-    }else if(rata>=85){
-        cout<<"selamat anda mendapatkan A";
-        }else if(rata>=70){
-        cout<<"selamat anda mendapatkan B";
-        }else if(rata>=60){
-        cout<<"Okelah anda mendapatkan C";
-        }else if(rata>=40){
-        cout<<"WOW anda mendapatkan D";
-        }else if(rata>=0){
-        cout<<"WOW anda mendapatkan F";
-        }else{
-            cout<<"rendehnya";
-        }
-
-    
-    
+string nama[5];
+for(int i=0; i<5;i++){
+    cout<<"Masukan nama "<<i+1<<" : ";
+    getline(cin,nama[i]);
+    cin.ignore();
+}
+cout<<"===Daftar nama==="<<endl;
+for(int a=0;a<5;a++){
+    cout<<a+1<<". "<<nama[a]<<endl;
+}
   return 0;
 }
