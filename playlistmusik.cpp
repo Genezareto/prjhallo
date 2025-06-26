bf2de800975455fed57df6d483c33e749aa8defc
Genezareto <genezaretokaijeputraguntur@gmail.com>
2025-06-26 11:29:10 +0800

    Tampilan awal

--- playlistmusik.cpp
+++ playlistmusik.cpp
@@ -16,7 +16,27 @@ int main() {
         cin >> pilihan;
         cin.ignore(); // Menghindari masalah getline setelah cin
 
-       
+        switch (pilihan) {
+            case 1:
+                cout << "Judul Lagu: ";
+                cout << "Penyanyi: ";
+                break;
+            case 2:
+                break;
+            case 3:
+                cout << "Masukkan judul lagu yang ingin dihapus: ";
+                break;
+            case 4:
+                cout << "Masukkan judul lagu yang dicari: ";
+                break;
+            case 5:
+                cout << "Keluar dari program.\n";
+                break;
+            default:
+                cout << "Pilihan tidak valid.\n";
+        }
+
+    } while (pilihan != 5);
+
     return 0;
-}
 }
